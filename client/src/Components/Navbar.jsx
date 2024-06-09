00216a24413b1fa47fef9e2360a7bbb2f23faa92
Sangita-M123 <153701193+Sangita-M123@users.noreply.github.com>
2024-06-09 17:57:41 +0530

    Updates Navbar.jsx

--- client/src/Components/Navbar.jsx
+++ client/src/Components/Navbar.jsx
@@ -1,76 +1,127 @@
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { logoutUser } from "../actions/userAction";
+import "./Navbar.css";
 export default function Navbar() {
   const cartstate = useSelector((state) => state.cartReducer);
   const userstate = useSelector((state) => state.loginUserReducer);
   const { currentUser } = userstate;
   const dispatch = useDispatch();
   return (
-    <div>
-      <nav className="navbar navbar-expand-lg shadow-lg p-3 mb-5 bg-white rounded">
+    <div className="navigator2 navbar-expand-lg shadow-lg p-3 mb-5 bg-white rounded">
+      <div className="title">
         <a className="navbar-brand" href="/">
           EAT STOL
         </a>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav ms-auto">
-            {currentUser ? (
-              <div class="dropdown mt-2">
-                <a
-                  style={{ color: "black" }}
-                  className="dropdown-toggle"
-                  type="button"
-                  data-bs-toggle="dropdown"
-                  aria-expanded="false"
-                >
-                  {currentUser.name}
-                </a>
-                <ul class="dropdown-menu">
-                  <li>
-                    <a className="dropdown-item" href="/orders">
-                      Order
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      className="dropdown-item"
-                      href="#"
-                      onClick={() => {
-                        dispatch(logoutUser());
-                      }}
-                    >
-                      <li>Logout</li>
-                    </a>
-                  </li>
-                </ul>
-              </div>
-            ) : (
-              <li className="nav-item active">
-                <a className="nav-link" href="/login">
-                  Login
+      </div>
+      <div className="options">
+        {currentUser ? (
+          <div class="dropdown">
+            <a
+              style={{ color: "black" }}
+              className="dropdown-toggle"
+              type="button"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+            >
+              {currentUser.name}
+            </a>
+            <ul class="dropdown-menu">
+              <li>
+                <a className="dropdown-item" href="/orders">
+                  Order
                 </a>
               </li>
-            )}
+              <li>
+                <a
+                  className="dropdown-item"
+                  href="#"
+                  onClick={() => {
+                    dispatch(logoutUser());
+                  }}
+                >
+                  <li>Logout</li>
+                </a>
+              </li>
+            </ul>
+          </div>
+        ) : (
+          <a className="nav-link" href="/login">
+            Login
+          </a>
+        )}
 
-            <li className="nav-item">
-              <a className="nav-link" href="/cart">
-                Cart {cartstate.cartItems.length}
-              </a>
-            </li>
-          </ul>
-        </div>
-      </nav>
+        <a className="nav-link" href="/cart">
+          Cart {cartstate.cartItems.length}
+        </a>
+      </div>
     </div>
   );
 }
+    // <div>
+    //   <nav className="navbar navbar-expand-lg shadow-lg p-3 mb-5 bg-white rounded">
+    //     <a className="navbar-brand" href="/">
+    //       EAT STOL
+    //     </a>
+    //     <button
+    //       className="navbar-toggler"
+    //       type="button"
+    //       data-toggle="collapse"
+    //       data-target="#navbarNav"
+    //       aria-controls="navbarNav"
+    //       aria-expanded="false"
+    //       aria-label="Toggle navigation"
+    //     >
+    //       <span className="navbar-toggler-icon"></span>
+    //     </button>
+    //     <div className="collapse navbar-collapse" id="navbarNav">
+    //       <ul className="navbar-nav ms-auto">
+    //         {currentUser ? (
+    //           <div class="dropdown mt-2">
+    //             <a
+    //               style={{ color: "black" }}
+    //               className="dropdown-toggle"
+    //               type="button"
+    //               data-bs-toggle="dropdown"
+    //               aria-expanded="false"
+    //             >
+    //               {currentUser.name}
+    //             </a>
+    //             <ul class="dropdown-menu">
+    //               <li>
+    //                 <a className="dropdown-item" href="/orders">
+    //                   Order
+    //                 </a>
+    //               </li>
+    //               <li>
+    //                 <a
+    //                   className="dropdown-item"
+    //                   href="#"
+    //                   onClick={() => {
+    //                     dispatch(logoutUser());
+    //                   }}
+    //                 >
+    //                   <li>Logout</li>
+    //                 </a>
+    //               </li>
+    //             </ul>
+    //           </div>
+    //         ) : (
+    //           <li className="nav-item active">
+    //             <a className="nav-link" href="/login">
+    //               Login
+    //             </a>
+    //           </li>
+    //         )}
+
+    //         <li className="nav-item">
+    //           <a className="nav-link" href="/cart">
+    //             Cart {cartstate.cartItems.length}
+    //           </a>
+    //         </li>
+    //       </ul>
+    //     </div>
+    //   </nav>
+    // </div>
+ // );
+//}
